6fd83ffcd0317f2b1943126d70cfa0e486b10455
DethPlague98 <tnaes98@gmail.com>
2026-06-26 08:25:07 -0500

    added essay

--- weather-app/app.js
+++ weather-app/app.js
@@ -46,3 +46,20 @@ function getWeather(city){
 
  console.log(chalk.blue(`City Temperature: ${weather.temperature}`));
  console.log(chalk.green(`City Conditions: ${weather.condition}`))
+
+ /*
+1. Purpose of package.json:
+   - package.json stores important project information, including the list of dependencies.
+   - It allows npm to know which modules the project needs.
+   - It ensures that anyone who downloads the project can install the correct dependencies.
+
+2. Why node_modules should NOT be included in version control:
+   - node_modules contains thousands of files and would not be a good way to store code efficiently
+   - All dependencies can be restored automatically using package.json.
+
+3. How `npm install` reinstalls dependencies and why it matters:
+   - When node_modules is deleted, running `npm install` reads package.json
+     and reinstalls every dependency listed.
+   - This is crucial for projects between multiple people because teammates can clone
+     the repo and run `npm install` to get the exact same environment.
+*/
